a2b1e1a5e3238c14b9a2edda07d144ea14686384
Sachko Honda <pickledcucumber@gmail.com>
2014-02-06 16:54:08 -0800

    initial checkin
    Coursera Android class provided source files and my homework 1,2,part3.

---
+++ week3/android-Labs-Intents-Intents/Intents/IntentsLab/IntentsLab/src/course/labs/intentslab/ExplicitlyLoadedActivity.java
@@ -0,0 +1,70 @@
+package course.labs.intentslab;
+
+import org.apache.http.protocol.HTTP;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ExplicitlyLoadedActivity extends Activity {
+
+	static private final String TAG = "Lab-Intents";
+	static public final String USER_TEXT = "UserText";
+	private EditText mEditText;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.explicitly_loaded_activity);
+
+		// Get a reference to the EditText field
+		mEditText = (EditText) findViewById(R.id.editText);
+
+		// Declare and setup "Enter" button
+		Button enterButton = (Button) findViewById(R.id.enter_button);
+		enterButton.setOnClickListener(new OnClickListener() {
+
+			// Call enterClicked() when pressed
+
+			@Override
+			public void onClick(View v) {
+
+				enterClicked();
+			
+			}
+		});
+
+	}
+
+	// Sets result to send back to calling Activity and finishes
+	
+	private void enterClicked() {
+
+		Log.i(TAG,"Entered enterClicked()");
+		
+		// TODO - Save user provided input from the EditText field
+		Editable text = mEditText.getText();
+		
+		// TODO - Create a new intent and save the input from the EditText field as an extra
+		Intent data = new Intent();
+		data.setType(HTTP.PLAIN_TEXT_TYPE);
+		data.putExtra(USER_TEXT, text.toString());
+		
+		// TODO - Set Activity's result with result code RESULT_OK
+		if (getParent() == null) {
+		    setResult(Activity.RESULT_OK, data);
+		} else {
+		    getParent().setResult(Activity.RESULT_OK, data);
+		}
+		// TODO - Finish the Activity
+		finish();
+	}
+}
